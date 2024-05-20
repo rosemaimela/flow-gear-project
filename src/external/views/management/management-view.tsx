781aa20b7a247543b38e01fec2ba84b28ad2fa29
Tiyani Mabunda <Tiyani.Mabunda@lifehealthcare.co.za>
2024-05-20 17:16:22 +0200

    Import excel

--- src/external/views/management/management-view.tsx
+++ src/external/views/management/management-view.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import * as xlsx from 'xlsx' ;
 import css from './management-view.module.css';
 import { useNavigate } from 'react-router-dom';
 import AppSearchInput from 'assets/bootstrap/input/search-input/search-input';
@@ -10,11 +11,32 @@ import AppNumberInput from 'assets/bootstrap/input/number-input/number-input';
 import AppButton from 'assets/bootstrap/button/button';
 import SaveIcon from 'assets/images/icons/save/save';
 
+
 const AppManagementPage = () => {
     const httpClientService: HttpClientService = new HttpClientService();
 
     const navigate = useNavigate();
 
+   
+    const importExcel = (e) => {
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(e.target.files[0]);
+        reader.onload = (e) =>{
+            const data = e.target.result;
+            const workbook = xlsx.read(data, {type : "array"});
+            const sheetName = workbook.SheetNames[0];
+            const sheet  = workbook.Sheets[sheetName];
+            const parseData = xlsx.utils.sheet_to_json(sheet);
+         httpClientService.post<Object,Object>( "/import/stock",parseData,{},true).then( (resp) => {
+
+         }).catch((error)=> {
+
+         }) ;
+
+        };
+
+    }
+   
     return (
         <main className={css.main}>
             <AppH2 className={css.h2} title='Add Stock' />
@@ -31,9 +53,11 @@ const AppManagementPage = () => {
                     <SaveIcon />
                 </AppButton>
             </div>
-            <AppButton className={css.import_excel_button}>
+            <AppButton className={css.import_excel_button} isImport = {true} onFileImport={importExcel} >
                 Import Excel
             </AppButton>
+            
+            
 
             <AppH2 className={css.h2} title='Get Current Stock Report' />
             <div className={css.date_range}>
